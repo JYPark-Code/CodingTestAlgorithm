6aeecd7d46494fa0d3475f038d8bc497612545e3
Ji Yong Park <toboy93@gmail.com>
2023-10-27 12:42:33 +0900

    [Silver V] Title: 뒤집기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -1,12 +1,8 @@
-S = input()
+numbers = input()
+count = 0
 
-bubble = [S[0]]
+for i in range(len(numbers) - 1):
+    if numbers[i] != numbers[i + 1]:
+        count += 1
 
-for i in range(len(S)-1):
-    if S[i] != S[i+1]:
-        bubble.append(S[i+1])
-
-zero = bubble.count("0")
-one = bubble.count("1")
-
-print(min(zero,one))
+print((count + 1) // 2)
