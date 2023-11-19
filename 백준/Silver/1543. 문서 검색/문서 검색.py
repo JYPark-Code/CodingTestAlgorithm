6f263a33ff56740269ee85aa9867aa5924955870
Ji Yong Park <toboy93@gmail.com>
2023-11-20 08:21:39 +0900

    [Silver V] Title: 문서 검색, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1543. 문서 검색/문서 검색.py
+++ 백준/Silver/1543. 문서 검색/문서 검색.py
@@ -1,5 +1,18 @@
-# 문서 탐색
+n = input()
+m = input()
 
-doc = input()
-word = input()
-print(doc.count(word))
+count = 0
+search = 0
+
+while search < len(n):
+    if n[search:(search + len(m))] == m and search + len(m) <= len(n):
+        count += 1
+        search += len(m)
+
+    elif n[search:(search + len(m))] != m and search + len(m) <= len(n):
+        search += 1
+
+    elif search + len(m) > len(n):
+        break
+
+print(count)
