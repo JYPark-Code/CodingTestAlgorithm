e9168313de21bb25e39f03faa3e56f30412c1b50
Ji Yong Park <toboy93@gmail.com>
2024-09-10 09:29:12 +0900

    [level 3] Title: 베스트앨범, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -0,0 +1,27 @@
+def solution(genres, plays):
+    
+
+    # 장르별로 곡과 재생 수를 담을 딕셔너리 생성
+    album = {}
+
+    # genres와 plays를 순회하며 album 딕셔너리 구성
+    for index, (genre, play) in enumerate(zip(genres, plays)):
+        if genre not in album:
+            album[genre] = []  # 해당 장르가 딕셔너리에 없을 경우 리스트로 초기화
+        album[genre].append((play, index))  # 재생 수와 인덱스를 해당 장르 리스트에 추가
+
+    # 장르별 총 재생 수를 계산하여 정렬
+    genre_play_count = {genre: sum(play for play, _ in plays) for genre, plays in album.items()}
+    sorted_genres = sorted(genre_play_count.keys(), key=lambda x: genre_play_count[x], reverse=True)
+
+    # 정답을 담을 리스트
+    result = []
+
+    # 정렬된 장르 순서대로 2곡씩 추출
+    for genre in sorted_genres:
+        # 해당 장르에서 재생 수가 많은 순으로 정렬 (재생 수가 같다면 인덱스 순으로 정렬)
+        top_songs = sorted(album[genre], key=lambda x: (-x[0], x[1]))
+        # 최대 두 곡의 인덱스를 result에 추가
+        result.extend([index for _, index in top_songs[:2]])
+
+    return result
