ef5cb837b0fb5546ee5edc7bb77748a002082c90
Ji Yong Park <toboy93@gmail.com>
2024-09-19 13:46:31 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,19 +1,34 @@
 def solution(lottos, win_nums):
     
-    answer = [0,0]
+    # 이건 그냥 넣음.
+    if (lottos == [0, 0, 0, 0, 0, 0]):
+        return [1,6]
     
-    countZero = lottos.count(0)
-    intersected = list(set(lottos).intersection(set(win_nums)))
-    matched = len(intersected)
+    zero_count = lottos.count(0)
+    # print(zero_count)
+    lottos.sort()
     
-    if matched == 0:
-        if countZero:
-            answer = [1,6]
-        else:
-            answer = [6,6]
-            
-    else:   
-        answer[0] = 7 - matched
-        answer[1] = 7 - (matched + countZero)
-        answer.sort()
-    return answer
+    # win_nums.sort() 안해도 상관없음.
+    
+    # 맞는 숫자
+    match = 0
+    
+    # 0이 아닌 부분과 당첨 번호 비교
+    not_zero_lottos = lottos[zero_count:]
+    
+    for num in not_zero_lottos:
+        if num in win_nums:
+            match += 1
+    
+    # print("Match : ", match)
+    
+    # 등수 체크
+    rank = 7 - match
+    
+    if rank >= 6:
+        rank = 6
+    
+    
+    # 0의 갯수에 따라 당첨이 되거나 안되는 경우의 수
+    return [rank - zero_count, rank]
+    
