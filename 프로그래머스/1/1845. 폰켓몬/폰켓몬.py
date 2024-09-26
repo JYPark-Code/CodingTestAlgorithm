5105803597c9d932b1ec0f9b57b149804a36f441
Ji Yong Park <toboy93@gmail.com>
2024-09-26 15:54:36 +0900

    [level 1] Title: 폰켓몬, Time: 0.78 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,19 +1,9 @@
 def solution(nums):
+    # 최대 선택 가능한 폰켓몬 수
+    available = len(nums) // 2
     
-    total = len(nums)
+    # 고유한 폰켓몬 종류
+    set_nums = set(nums)
     
-    max_choice = total // 2
-    
-    sorted_nums = set(nums)
-    
-    # print(sorted_nums)
-    
-    if len(sorted_nums) > max_choice:    
-        return max_choice
-    
-    else:
-        return len(sorted_nums)
-        
-    
-    # answer = 0
-    # return answer
+    # 고유한 폰켓몬 종류 수와 선택 가능한 수 중 더 작은 값 반환
+    return min(len(set_nums), available)
