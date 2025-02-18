761b2ee08b993952f3c157a8ca203360240e6274
Ji Yong Park <toboy93@gmail.com>
2025-02-18 16:40:18 +0900

    [level 2] Title: 구명보트, Time: 9.06 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,15 @@
+# 복잡도 N log N
+def solution(people, limit):
+    
+    people.sort()  # 몸무게를 오름차순으로 정렬
+    left = 0
+    right = len(people) - 1
+    answer = 0
+    
+    while left <= right:
+        if people[left] + people[right] <= limit:
+            left += 1  # 가장 가벼운 사람도 함께 태움
+        right -= 1  # 무거운 사람은 무조건 배에 태우기
+        answer += 1  # 배 사용 횟수 증가
+
+    return answer
