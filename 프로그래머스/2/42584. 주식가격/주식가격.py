4b0fd49b3a312a4d4f9c4bf1116f4d64935416a0
Ji Yong Park <toboy93@gmail.com>
2025-04-14 15:17:10 +0900

    [level 2] Title: 주식가격, Time: 24.00 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,18 +1,17 @@
 def solution(prices):
-    stack = []
-    answer = [0] * len(prices)  # 정답 배열 미리 생성 (초기값 0)
+    answer = [0] * len(prices)
+    stack = []  # (index, price)
 
-    for i in range(len(prices)):
-        # 가격이 떨어지는 경우, 스택에서 pop하면서 지속 시간 계산
-        while stack and prices[i] < prices[stack[-1]]:
-            j = stack.pop()  # 가격이 유지되지 못한 인덱스 꺼내기
-            answer[j] = i - j  # 가격이 유지된 시간 계산
-        
-        stack.append(i)  # 현재 인덱스 추가
-    
-    # 스택에 남아 있는 요소들은 끝까지 가격이 유지된 값들
+    for current_time, current_price in enumerate(prices):
+        # 스택에 있는 이전 가격들보다 현재 가격이 낮으면, 떨어진 걸로 간주
+        while stack and current_price < prices[stack[-1]]:
+            past_time = stack.pop()
+            answer[past_time] = current_time - past_time
+        stack.append(current_time)
+
+    # 아직 떨어지지 않은 항목들 처리 (끝까지 유지된 가격)
     while stack:
-        j = stack.pop()
-        answer[j] = len(prices) - 1 - j  # 끝까지 유지된 경우 처리
-    
+        past_time = stack.pop()
+        answer[past_time] = len(prices) - 1 - past_time
+
     return answer
