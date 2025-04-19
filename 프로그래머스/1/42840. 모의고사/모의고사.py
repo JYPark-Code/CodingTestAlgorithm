e15bf73906c9c72e0c9675c9d6ad392af2ace6c3
Ji Yong Park <toboy93@gmail.com>
2025-04-19 15:42:56 +0900

    [level 1] Title: 모의고사, Time: 5.13 ms, Memory: 9.37 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,36 +1,22 @@
 def solution(answers):
-    # 패턴
-    a = [1, 2, 3, 4, 5] # 5
-    b = [2, 1, 2, 3, 2, 4, 2, 5] # 8
-    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] # 10
     
-    ac = 0
-    bc = 0
-    cc = 0
+    pattern = {
+        1:[1, 2, 3, 4, 5],
+        2:[2, 1, 2, 3, 2, 4, 2, 5],
+        3:[3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    }
+    
+    matched = [0] * 4
+    
+    for guy in pattern :
+        for idx, ans in enumerate(answers):
+            if pattern[guy][idx % (len(pattern[guy]))] == ans:
+                matched[guy] += 1
     
     answer = []
     
-    for index, num in enumerate(answers):
-        if num == a[index % len(a)]:
-            ac += 1
-            
-        if num == b[index % len(b)]:
-            bc += 1
-            
-        if num == c[index % len(c)]:
-            cc += 1
-        
-    # 최대 정답 수 계산
-    max_num = max(ac, bc, cc)
+    for idx, i in enumerate(matched): 
+        if i == max(matched):
+            answer.append(idx)
     
-    # 가장 많이 맞춘 사람의 번호 추가
-    if max_num == ac:
-        answer.append(1)
-        
-    if max_num == bc:
-        answer.append(2)
-        
-    if max_num == cc:
-        answer.append(3)
-        
     return answer
