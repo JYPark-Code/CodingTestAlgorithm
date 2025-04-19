5d53e0b7a49629a49a9b776ade5da539f0f46e01
Ji Yong Park <toboy93@gmail.com>
2025-04-19 13:46:39 +0900

    [level 3] Title: 입국심사, Time: 588.21 ms, Memory: 13.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/README.md
+++ 프로그래머스/3/43238. 입국심사/README.md
@@ -1,10 +1,10 @@
 # [level 3] 입국심사 - 43238 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238?itm_content=course14743) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43238) 
 
 ### 성능 요약
 
-메모리: 14.2 MB, 시간: 1280.79 ms
+메모리: 13.5 MB, 시간: 588.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 24일 15:19:33
+2025년 04월 19일 13:46:31
 
 ### 문제 설명
 
--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,15 +1,25 @@
 def solution(n, times):
-    left, right = 1, max(times) * n
-    answer = right  # ✅ 초기값은 가능한 최대값으로 설정
-
+    
+    # n명 대기
+    # times = 입국 심사 심사대(index), 심사대 당 걸리는 시간
+    
+    left = 1
+    right = max(times) * n
+    
+    answer = 0
+    
     while left <= right:
-        mid = (left + right) // 2
-        total = sum(mid // t for t in times)
-
-        if total >= n:  # 가능한 경우
-            answer = mid  # ✅ 가능한 값으로 answer 갱신
-            right = mid - 1  # 더 작은 시간 찾기
-        else:  # 불가능한 경우
+        
+        mid = (right + left) // 2
+        
+        total = sum( mid // t for t in times)
+        
+        if total >= n:
+            answer = mid
+            right = mid - 1
+        
+        else:
             left = mid + 1
-
+        
+    
     return answer
