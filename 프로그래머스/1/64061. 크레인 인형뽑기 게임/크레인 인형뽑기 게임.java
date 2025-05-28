0c1d0b2acf19d590a0fe7724a35283c9feda53c3
Ji Yong Park <toboy93@gmail.com>
2025-05-28 16:12:48 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.33 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(int[][] board, int[] moves) {
+        
+        Stack<Integer> stack = new Stack();
+        
+        int answer = 0;
+        
+        for(int move : moves) {
+            
+            int col = move - 1;
+         
+            for (int row = 0 ; row < board.length; row++){
+                
+                int doll = board[row][col];
+                if(doll != 0){
+                    board[row][col] = 0;
+                
+                
+                    if(!stack.isEmpty() && stack.peek() == doll){
+                        stack.pop();
+                        answer += 2;
+                    } else {
+                        stack.push(doll);
+                    }
+                break;
+                }
+            } 
+        }
+    
+        return answer;
+    }
+}
