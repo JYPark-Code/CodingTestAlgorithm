94651eb9093525b96f99bcedf6d2b7c23db73e69
Ji Yong Park <toboy93@gmail.com>
2025-09-29 15:31:48 +0900

    [level 2] Title: 올바른 괄호, Time: 8.55 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,14 +1,18 @@
-def solution(s):
+def solution(string):
     answer = True
     stack = []
-    
-    for i in s:
-        stack.append(i)
-        
-        if len(stack) >= 2 and stack[-2:] == ['(', ')']:
-            del stack[-2:]
-    
-    if len(stack) > 0:
-        return False
-    
-    return True
+
+    for char in string:
+        if char == '(':
+            stack.append(char)
+
+        elif char == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+                continue
+            stack.append(char)
+
+    if stack:
+        answer = False
+
+    return answer
