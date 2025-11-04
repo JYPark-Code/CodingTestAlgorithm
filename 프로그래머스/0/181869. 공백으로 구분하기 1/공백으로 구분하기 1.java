cfd965613a4b8ab184260468ef98b299e58228d3
Ji Yong Park <toboy93@gmail.com>
2025-11-04 17:20:26 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.30 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -1,7 +1,7 @@
 import java.util.*;
 class Solution {
     public List<String> solution(String my_string) {
-        List<String> answer = Arrays.asList(my_string.split(" "));
+        List<String> answer = new ArrayList<>(Arrays.asList(my_string.split(" ")));
         return answer;
     }
 }
