9871ffb499ee170c988e683fa358eb891d75bf8f
Ji Yong Park <toboy93@gmail.com>
2025-11-06 17:57:43 +0900

    [level 0] Title: 리스트 자르기, Time: 2.98 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,28 +1,5 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        
-        int a = slicer[0];
-        int b = slicer[1];
-        int c = slicer[2];
-        
-        
-        if(n == 1){
-            return Arrays.copyOfRange(num_list, 0, b + 1);
-        } else if(n == 2){
-            return Arrays.copyOfRange(num_list, a, num_list.length);
-        } else if(n == 3){
-            return Arrays.copyOfRange(num_list, a, b + 1);
-        } else if (n == 4){
-            List<Integer> tmp = new ArrayList<>();
-            for (int i = a; i <= b; i += c) {
-                tmp.add(num_list[i]);
-            }
-            return tmp.stream().mapToInt(Integer::intValue).toArray();
-        }
-        
-        
         int[] answer = {};
         return answer;
     }
