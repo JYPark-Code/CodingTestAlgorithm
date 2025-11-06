3d13d2b90f56010a7e3a7b21e94c49656f73f030
Ji Yong Park <toboy93@gmail.com>
2025-11-06 17:10:35 +0900

    [level 0] Title: 세 개의 구분자, Time: 54.86 ms, Memory: 95.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myStr) {
+        String[] parts = myStr.split("[abc]+");
+        
+        String[] answer = Arrays.stream(parts)
+            .filter(s -> !s.isEmpty())
+            .toArray(String[]::new);
+        
+        if (answer.length == 0){
+            return new String[] {"EMPTY"};
+        }
+
+        return answer;
+    }
+}
