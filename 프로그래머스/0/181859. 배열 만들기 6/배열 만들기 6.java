124de830705d9a38308320b8d79c205b97c8c25f
Ji Yong Park <toboy93@gmail.com>
2025-11-07 15:03:06 +0900

    [level 0] Title: 배열 만들기 6, Time: 46.70 ms, Memory: 141 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            if (answer.isEmpty()){
+                answer.add(arr[i]);
+            } else {
+                int last = answer.get(answer.size() - 1);
+                if(last == arr[i]) {
+                    answer.remove(answer.size() - 1);
+                } else {
+                    answer.add(arr[i]);
+                }
+            }   
+        }
+        
+        if(answer.isEmpty()){
+            return new ArrayList<>(Arrays.asList(-1));
+        }
+        
+        return answer;
+    }
+}
