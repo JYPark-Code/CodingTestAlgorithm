5d3c0401c1d71da68333bef474faef13947c5363
Ji Yong Park <toboy93@gmail.com>
2025-11-14 12:37:49 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.04 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.java
@@ -13,20 +13,15 @@ class Solution {
         
         for (int i = 0; i < last_attack; i++){
             
-            // System.out.println(i + "번 공격 예정, " + "현재 체력 : " + current_health);
+            System.out.println(i + "번 공격 예정, " + "현재 체력 : " + current_health);
             
             int gap_time = attacks[i+1][0] - attacks[i][0];
             
-            // System.out.println("다음 공격까지 : " + gap_time);
-            
-            // 즉사
-            if ( (current_health - attacks[i][1]) <= 0){
-                return -1;
-            }
+            System.out.println("다음 공격까지 : " + gap_time);
             
             if (gap_time > 1){
                 
-//                 System.out.println("현재 체력 : " + current_health + " - 현재 공격 : " + attacks[i][1] + " + 힐 예정량 : " + heal(bandage, gap_time));
+                System.out.println("현재 체력 : " + current_health + " - 현재 공격 : " + attacks[i][1] + " + 힐 예정량 : " + heal(bandage, gap_time));
                 
                 current_health = current_health - attacks[i][1] + 
                 heal(bandage, gap_time);
@@ -34,11 +29,11 @@ class Solution {
             
             
             else{
-                // System.out.println("현재 체력 : " + current_health + " - 현재 공격 : " + attacks[i][1]);
+                System.out.println("현재 체력 : " + current_health + " - 현재 공격 : " + attacks[i][1]);
                 current_health -= attacks[i][1];
                 }
             
-            // System.out.println(i + "번째 공격, 현재 체력: " + current_health + " 입은 데미지 : " + attacks[i][1]);
+            System.out.println(i + "번째 공격, 현재 체력: " + current_health + " 입은 데미지 : " + attacks[i][1]);
             
             if (current_health <= 0)
                 return -1;
@@ -50,7 +45,7 @@ class Solution {
         
         current_health -= attacks[last_attack][1];
         
-        // System.out.println("마지막 공격, 현재 체력 : " + current_health + " 입은 데미지 : " + attacks[last_attack][1]);
+        System.out.println("마지막 공격, 현재 체력 : " + current_health + " 입은 데미지 : " + attacks[last_attack][1]);
         
         if (current_health <= 0)
             return -1;
@@ -64,18 +59,10 @@ class Solution {
         int process_time = bandage[0];
         int heal_amount = bandage[1];
         int bonus_heal = bandage[2];
-        int healSecond = time - 1;
-
-        if (healSecond <= 0){
-            return 0;
-        }
-
-        int total = heal_amount * healSecond;
-
-        int fullCasts = healSecond / process_time;
-
-        total += fullCasts * bonus_heal;
-
-        return total;
+        
+        if (time >= process_time)
+            return (heal_amount * (time - 1)) + (time / process_time) * bonus_heal; 
+        else
+            return (heal_amount * (time - 1));
     }
 }
