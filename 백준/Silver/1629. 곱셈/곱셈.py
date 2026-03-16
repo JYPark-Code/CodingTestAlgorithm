465d19f8394c8db34f18275459a19e3a80bab9aa
Ji Yong Park <toboy93@gmail.com>
2026-03-16 14:02:31 +0900

    [Silver I] Title: 곱셈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -3,5 +3,22 @@ input = sys.stdin.readline
 # a^b % c ?
 a, b, c = map(int,(input().split()))
 
-answer = pow(a, b, c)
-print(answer) 
+# answer = pow(a, b, c)
+# print(answer)
+
+if b == 0:
+    print(1)
+    exit()
+
+answer = 1
+
+while b > 0:
+
+    if b % 2 == 1:
+        answer = (answer * a) % c
+
+    a = (a * a) % c
+
+    b //= 2
+
+print(answer)
