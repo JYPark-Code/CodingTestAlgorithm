f47481c9e0d752c91d576a16dc9df9fad95cb0e6
Ji Yong Park <toboy93@gmail.com>
2026-03-27 21:35:25 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,27 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+if n == 0:
+    print(0)
+    exit()
+if n == 1:
+    print(1)
+    exit()
+if n == 2:
+    print(1)
+    exit()
+
+if n >= 3:
+    dp = [0] * (n+1)
+
+dp[0] = 0
+dp[1] = 1
+dp[2] = 1
+
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[n])
